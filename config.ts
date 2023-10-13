93e88080f558a6a2731470ef371906be9c0f3c2b
Ed Mazurek <changes@gmail.com>
2023-10-13 11:07:23 +0100

    Change networks to goerli, OP goerli (#17)

--- config.ts
+++ config.ts
@@ -1,18 +1,18 @@
-import { avalancheFuji, polygonMumbai } from "wagmi/chains";
+import { goerli, optimismGoerli } from "wagmi/chains";
 
-export const chains = [avalancheFuji, polygonMumbai];
+export const chains = [goerli, optimismGoerli];
 
 const config = {
   l1: {
-    chain: avalancheFuji,
-    wormholeChainId: 5,
-    tokenAddress: "0xeA7448e198B00E0162Cd1238B5a4A561d9e69A3e" as `0x${string}`,
-    erc20Bridge: "0x89A40dd9fe5a04735DDDafe555340327889ae0f0" as `0x${string}`,
+    chain: goerli,
+    wormholeChainId: 2,
+    tokenAddress: "0x02CD4A674A29973911D5D291b14caB75b267062c" as `0x${string}`,
+    erc20Bridge: "0x127b4cB434E2bFA5e67B8b7fAd15662972597eb3" as `0x${string}`,
   },
   l2: {
-    chain: polygonMumbai,
-    wormholeChainId: 6,
-    tokenAddress: "0x32E99074C339d5659C6787EE1E627E4036827725" as `0x${string}`,
+    chain: optimismGoerli,
+    wormholeChainId: 24,
+    tokenAddress: "0x74975D71A63D0d955160E38bf427dA7f3b030888" as `0x${string}`,
   },
 };
 
