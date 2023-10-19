f3a9c57cc8bcea9329bdfaa1c4ff34c7e99287cb
wildmolasses <changes@gmail.com>
2023-10-19 12:10:12 +0200

    Update config to latest

--- config.ts
+++ config.ts
@@ -6,13 +6,13 @@ const config = {
   l1: {
     chain: goerli,
     wormholeChainId: 2,
-    tokenAddress: "0x02CD4A674A29973911D5D291b14caB75b267062c" as `0x${string}`,
-    erc20Bridge: "0x127b4cB434E2bFA5e67B8b7fAd15662972597eb3" as `0x${string}`,
+    tokenAddress: "0x10d1B06c702F24B8d1fF6d2Bec57Ae4fC62b7A86" as `0x${string}`,
+    erc20Bridge: "0xD217A95FdEaD17eE0C80361E1eb2601131F3Da9A" as `0x${string}`,
   },
   l2: {
     chain: optimismGoerli,
     wormholeChainId: 24,
-    tokenAddress: "0x74975D71A63D0d955160E38bf427dA7f3b030888" as `0x${string}`,
+    tokenAddress: "0xaF2E86479a6181A30F411c35eF946Ff22b15b2dc" as `0x${string}`,
   },
   showDebug: true,
 };
