2fdb1f32edc99e9d5e5b72e8bfffa0500d688acc
keating <keating.dev@protonmail.com>
2023-10-21 23:30:47 -0400

    React table wip

--- config.ts
+++ config.ts
@@ -4,6 +4,7 @@ export const chains = [goerli, optimismGoerli];
 
 export enum DaoId {
   Example = 1,
+  ExampleComp = 2,
 }
 
 export const config = {
@@ -13,15 +14,39 @@ export const config = {
     l1: {
       chain: goerli,
       wormholeChainId: 2,
-      tokenAddress: '0x10d1B06c702F24B8d1fF6d2Bec57Ae4fC62b7A86 ' as `0x${string}`,
+      tokenAddress: '0x10d1B06c702F24B8d1fF6d2Bec57Ae4fC62b7A86' as `0x${string}`,
       erc20Bridge: '0xD217A95FdEaD17eE0C80361E1eb2601131F3Da9A' as `0x${string}`,
+			governor: '0xF93BaC2e1a0f355Cd4fC4C9175eA56FF32532546' as `0x${string}`,
+			metadataBridge: '0x480fC82eD089193486ad2Ac860393A3Eb11c93A9' as `0x${string}`,
     },
     l2: {
       chain: optimismGoerli,
       wormholeChainId: 24,
       tokenAddress: '0xaF2E86479a6181A30F411c35eF946Ff22b15b2dc' as `0x${string}`,
+			voteAggregator: '0x74f00907CFC6E44Fb72535cdD1eC52a37EacAbE4' as `0x${string}`,
       logoUri:
         'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
     },
   },
+ [DaoId.ExampleComp]: {
+    name: 'Example Governor Comp',
+    showDebug: true,
+    l1: {
+      chain: goerli,
+      wormholeChainId: 2,
+      tokenAddress: '0xc7df884825f10bcA8043BBb88b8477D4Cd33Fba0' as `0x${string}`,
+      erc20Bridge: '0x8D5B3952A5F7Bd51714271Ec92379855595D9DE5' as `0x${string}`,
+			governor: '0xBDEc5BBD2a1c9Ef75e77b61738258D6dD7d846C0' as `0x${string}`,
+			metadataBridge: '0x42D6dDb7EE20D48A0bD678B333D516052b1747D2' as `0x${string}`,
+    },
+    l2: {
+      chain: optimismGoerli,
+      wormholeChainId: 24,
+      tokenAddress: '0x28c3C9A98c91f1D33FA65E8591b2c9024F26FbD1' as `0x${string}`,
+			voteAggregator: '0xbE51ED9444CECD4cef070E13F6fe03E38dE49Bb2' as `0x${string}`,
+      logoUri:
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+    },
+  },
+
 };
--- pages/vote.tsx
+++ pages/vote.tsx
@@ -1,14 +1,94 @@
+import {useMemo} from "react"
 import type { NextPage } from "next";
 import Head from "next/head";
+import {
+  Column,
+  Table as ReactTable,
+  PaginationState,
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  ColumnDef,
+	createColumnHelper,
+  OnChangeFn,
+  flexRender,
+} from '@tanstack/react-table'
+import { ChevronLeftIcon, ChevronRightIcon, ChevronDoubleRightIcon, ChevronDoubleLeftIcon } from '@heroicons/react/20/solid'
 
-// replace peopole with proposals
-const people = [
-  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+const data = [
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'Boo', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
+  { proposal: 'First proposal', startBlock: 9889767, endBlock: 9989767, votingPower: 1, isCancelled: false, tallyLink: "https://www.tally.xyz/gov/l2-flexible-voting-l2-example-4/proposal/89270641089180915787469790455189606853722485468293246956123979295426618605092"  },
 ]
 
+type Proposal = {
+	proposal: string;
+	startBlock: number;
+	endBlock: number;
+	votingPower: number;
+	isCancelled: boolean;
+	tallyLink: string;
+}
 
+
+// proposal information
+//
+// 1. Name of the proposal
+// 2. Start date
+// 3. L2 voting end date
+// 4. users voting power
+// 5. has the proposal been cancelled (the l2 state of the proposal more generally)
+// 6 link to the tally page for a user to vote
 const Vote: NextPage = () => {
+	const columnHelper = createColumnHelper<Proposal>()
+const columns = [
+  columnHelper.accessor('proposal', {
+    cell: info => info.getValue(),
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor(row => row.startBlock, {
+    id: 'starBlock',
+    cell: info => <i>{info.getValue()}</i>,
+    header: () => <span>Last Name</span>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('endBlock', {
+    header: () => 'Age',
+    cell: info => info.renderValue(),
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('votingPower', {
+    header: () => <span>Visits</span>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('isCancelled', {
+    header: 'Status',
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('tallyLink', {
+    header: 'Profile Progress',
+    footer: info => info.column.id,
+  }),
+]
   return (
     <>
       <Head>
@@ -18,53 +98,214 @@ const Vote: NextPage = () => {
 		    <h1>Proposals</h1>
 		   <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                      Name
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Title
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Email
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Role
-                    </th>
-                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                      <span className="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  {people.map((person) => (
-                    <tr key={person.email}>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                        {person.name}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.title}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.email}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.role}</td>
-                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                          Edit<span className="sr-only">, {person.name}</span>
-                        </a>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+       <Table
+        {...{
+          data,
+          columns,
+        }}
+      />
         </div>
       </div>
       </div>
     </>
   );
 };
+
+function Table({
+  data,
+  columns,
+}: {
+  data: Proposal[]
+  columns: ColumnDef<Proposal>[]
+}) {
+  const table = useReactTable({
+    data,
+    columns,
+    // Pipeline
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    //
+    debugTable: true,
+  })
+
+  return (
+    <div className="p-2">
+      <div className="h-2" />
+      <table className="min-w-full divide-y divide-gray-300">
+        <thead className="bg-gray-50">
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id} scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+     {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id} className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="h-2" />
+		  <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="flex flex-1 justify-between sm:hidden">
+        <a
+          href="#"
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Previous
+        </a>
+        <a
+          href="#"
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Next
+        </a>
+      </div>
+      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p className="text-sm text-gray-700">
+            Showing <span className="font-medium">{table.getState().pagination.pageIndex + 1} </span> of{' '}{table.getPageCount()}
+          </p>
+        </div>
+        <div>
+          <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+            <div className="flex items-center gap-2">
+               <button
+                 className="border rounded p-1 cursor-pointer"
+                 onClick={() => table.setPageIndex(0)}
+                 disabled={!table.getCanPreviousPage()}
+               >
+                 <ChevronDoubleLeftIcon className="h-5 w-5" aria-hidden="true" />
+               </button>
+               <button
+                 className="border rounded p-1 cursor-pointer"
+                 onClick={() => table.previousPage()}
+                 disabled={!table.getCanPreviousPage()}
+               >
+                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+               </button>
+               <button
+                 className="border rounded p-1 cursor-pointer"
+                 onClick={() => table.nextPage()}
+                 disabled={!table.getCanNextPage()}
+               >
+                 <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+               </button>
+               <button
+                 className="border rounded p-1 cursor-pointer"
+                 onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                 disabled={!table.getCanNextPage()}
+               >
+                 <ChevronDoubleRightIcon className="h-5 w-5" aria-hidden="true" />
+               </button>
+             </div>
+            <span className="flex items-center gap-4 px-2">
+              <div>Go to:</div>
+              <input
+                type="number"
+		            min="0"
+                defaultValue={table.getState().pagination.pageIndex + 1}
+                onChange={e => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0
+									console.log(page)
+									if (page > table.getPageCount() - 1) {
+										return
+									}
+                  table.setPageIndex(page)
+                }}
+                className="border p-1 rounded w-16"
+              />
+            </span>
+            <span className="flex items-center gap-4 px-2">
+              <div>Size:</div>
+              <select
+                  value={table.getState().pagination.pageSize}
+                  onChange={e => {
+                    table.setPageSize(Number(e.target.value))
+                  }}
+		              className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                {[10, 20, 30, 40, 50].map(pageSize => (
+                  <option key={pageSize} value={pageSize}>
+                    {pageSize}
+                  </option>
+                ))}
+              </select>
+            </span>
+          </nav>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+// function Filter({
+//   column,
+//   table,
+// }: {
+//   column: Column<any, any>
+//   table: ReactTable<any>
+// }) {
+//   const firstValue = table
+//     .getPreFilteredRowModel()
+//     .flatRows[0]?.getValue(column.id)
+// 
+//   const columnFilterValue = column.getFilterValue()
+// 
+//   return typeof firstValue === 'number' ? (
+//     <div className="flex space-x-2">
+//       <input
+//         type="number"
+//         value={(columnFilterValue as [number, number])?.[0] ?? ''}
+//         onChange={e =>
+//           column.setFilterValue((old: [number, number]) => [
+//             e.target.value,
+//             old?.[1],
+//           ])
+//         }
+//         placeholder={`Min`}
+//         className="w-24 border shadow rounded"
+//       />
+//       <input
+//         type="number"
+//         value={(columnFilterValue as [number, number])?.[1] ?? ''}
+//         onChange={e =>
+//           column.setFilterValue((old: [number, number]) => [
+//             old?.[0],
+//             e.target.value,
+//           ])
+//         }
+//         placeholder={`Max`}
+//         className="w-24 border shadow rounded"
+//       />
+//     </div>
+//   ) : (
+//     <input
+//       type="text"
+//       value={(columnFilterValue ?? '') as string}
+//       onChange={e => column.setFilterValue(e.target.value)}
+//       placeholder={`Search...`}
+//       className="w-36 border shadow rounded"
+//     />
+//   )
+// }
+
 
 export default Vote;
