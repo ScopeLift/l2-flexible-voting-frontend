28b772118fb42783362f94ab7ac3377f45576943
garyghayrat <gary.ghayrat@pm.me>
2023-10-30 20:52:50 -0400

    Remove unused imports

--- components/Notifications.tsx
+++ components/Notifications.tsx
@@ -7,13 +7,11 @@ import {
 } from '@heroicons/react/24/outline';
 import { XMarkIcon } from '@heroicons/react/20/solid';
 import { useNotifications } from '@/contexts/NotificationsContext';
-import { useNetwork } from 'wagmi';
 import PillAction from '@/components/PillAction';
 import { truncateHash, getChain, getEtherscanUrl } from '@/util';
 
 export default function Notifications() {
   const { notifications, dismiss } = useNotifications();
-  const { chain } = useNetwork();
 
   return (
     <>
